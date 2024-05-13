01e3a663017a67dd9907d01f2ae344b4351a519f
pelyubov <thechosenonenothanthanh2003@gmail.com>
2024-05-13 22:57:40 +0700

    channel refactor

--- src/entities/channel.entity.ts
+++ src/entities/channel.entity.ts
@@ -1,15 +1,11 @@
-import { UserId } from '@Project.Utils/types';
+import { ChannelId, UserId } from '@Project.Utils/types';
 
 export class Channel {
-  id: bigint;
-  members: bigint[];
-  name?: string;
-  photo?: string;
-  messages?: bigint[];
-  constructor(id: bigint, members: UserId[], name?: string, photo?: string) {
-    this.id = id;
-    this.name = name;
-    this.photo = photo;
-    this.members = members;
-  }
+  constructor(
+    public readonly id: ChannelId,
+    public ownerId: UserId,
+    public name: string,
+    public members: bigint[],
+    public photo?: string
+  ) {}
 }
