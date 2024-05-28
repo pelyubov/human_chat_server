974e13e1faeb4f0fa075eae78fe8f74ed981bf3a
Sayuri <54628820+sayuriu@users.noreply.github.com>
2024-05-28 20:11:29 +0700

    added challenge capability via websocket

--- src/ws/ws.gateway.ts
+++ src/ws/ws.gateway.ts
@@ -16,7 +16,7 @@ import { ChannelManagerService } from '@Project.Managers/channel-manager.service
 import { Long } from '@Project.Utils/types';
 
 interface IUserSession extends WebSocket {
-  clientId: UUID;
+  socketId: UUID;
   userId: bigint;
   currentChannel?: bigint;
 }
@@ -27,6 +27,7 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() private readonly server: WsServer;
 
   private readonly clients = new Map<bigint, Map<UUID, IUserSession>>();
+  private readonly challengeTimeouts = new Map<UUID, NodeJS.Timeout>();
   constructor(
     @Inject(forwardRef(() => ChannelManagerService))
     private readonly channels: ChannelManagerService,
@@ -36,34 +37,88 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.logger.log('WsGateway initialized', 'WsGateway');
   }
 
+  async authChallenge(client: IUserSession) {
+    client.send(
+      JSON.stringify({
+        event: 'tokenChallenge'
+      })
+    );
+    this.challengeTimeouts.set(
+      client.socketId,
+      setTimeout(() => {
+        client.send(
+          JSON.stringify({
+            event: 'tokenChallengeTimeout'
+          })
+        );
+        client.close();
+        this.logger.log(
+          `Client(${client.socketId}) has been disconnected due lack of response.`,
+          'WsGateway'
+        );
+      }, 10000)
+    );
+  }
+
+  @SubscribeMessage('tokenResponse')
+  async onTokenResponse(
+    @ConnectedSocket() client: IUserSession,
+    @MessageBody('token') token: string
+  ) {
+    try {
+      const { userId } = await this.auth.verify(token);
+      client.userId = userId.toBigInt();
+      this.logger.log(`Client(${client.socketId}) passed the token challenge.`, 'WsGateway');
+      client.send(
+        JSON.stringify({
+          event: 'tokenAccepted'
+        })
+      );
+      clearTimeout(this.challengeTimeouts.get(client.socketId)!);
+      this.onConnectSucessful(client, userId.toBigInt());
+    } catch (e) {
+      client.send(
+        JSON.stringify({
+          event: 'tokenRejected'
+        })
+      );
+      this.logger.error(`${e.message}, ${e.message}: ${token}`, 'WsGateway.Unauthorized');
+      client.close();
+    }
+  }
+
   async handleConnection(client: IUserSession, message: IncomingMessage) {
     const token = message.headers.authorization;
+    const socketId = randomUUID();
+    client.socketId = socketId;
+
     if (!token) {
-      this.logger.error(`${message.headers.origin} has no token`, 'WsGateway.Unauthorized');
-      client.close();
+      this.logger.log(`Client(${client.socketId}) has no token`, 'WsGateway.Unauthorized');
+      this.logger.log(`Sending challenge to Client(${client.socketId})`, 'WsGateway');
+      this.authChallenge(client);
       return;
     }
     try {
       const userId = (await this.auth.verify(token)).userId.toBigInt();
-      const socketId = randomUUID();
-      client.userId = userId;
-      client.clientId = socketId;
-
-      if (this.clients.has(userId)) {
-        this.clients.get(userId)!.set(socketId, client);
-      } else {
-        this.clients.set(userId, new Map([[socketId, client]]));
-      }
-
-      this.logger.log(`Client(${socketId}) [User(${userId})] has connected.`, 'WsGateway');
+      this.onConnectSucessful(client, userId);
     } catch (e) {
       this.logger.error(`${e.message}, token: ${token}`, 'WsGateway.Unauthorized');
       client.close();
     }
   }
 
+  onConnectSucessful(client: IUserSession, userId: bigint) {
+    client.userId = userId;
+    if (this.clients.has(userId)) {
+      this.clients.get(userId)!.set(client.socketId, client);
+    } else {
+      this.clients.set(userId, new Map([[client.socketId, client]]));
+    }
+    this.logger.log(`Client(${client.socketId}) [User(${userId})] has connected.`, 'WsGateway');
+  }
+
   async handleDisconnect(client: IUserSession) {
-    const { userId, clientId } = client;
+    const { userId, socketId: clientId } = client;
     if (!this.clients.has(userId)) {
       return;
     }
@@ -75,7 +130,7 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     for (const userId of users) {
       if (!this.clients.has(userId)) continue;
       for (const client of this.clients.get(userId)!.values()) {
-        if (exclude?.has(client.clientId)) continue;
+        if (exclude?.has(client.socketId)) continue;
         client.send(
           JSON.stringify({ event, data }, (_, value) =>
             typeof value === 'bigint' ? value.toString() : value
